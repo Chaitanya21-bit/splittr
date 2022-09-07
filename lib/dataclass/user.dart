85e519d6e3d71c95fede8f8c5c136410755ba78e
Saurrabhh <saurabhkyadav99@gmail.com>
2022-09-07 23:32:48 +0530

    login and signup

---
+++ lib/dataclass/user.dart
@@ -0,0 +1,7 @@
+class User {
+  late String name;
+  late String alias;
+  late String email;
+
+  User({required this.name, required this.alias, required this.email});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:splitter/screens/auth_screens/login_screen.dart';
 import 'auth/firebase_options.dart';
 import 'auth/firebase_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseManager();
   runApp(const MyApp());
 }
 
@@ -14,6 +17,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FirebaseAuth auth = FirebaseManager.auth;
+
+    if (auth.currentUser == null) {
+      return MaterialApp(
+        title: 'Login',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: LoginScreen(),
+      );
+    }
     return MaterialApp(
       title: 'FlDemo',
       theme: ThemeData(
@@ -38,14 +52,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    FirebaseManager();
   }
 
   Future<void> _incrementCounter() async {
-    print("after");
-    print(FirebaseManager.auth);
-    final snapshot = await FirebaseManager.database.ref().child("data").get();
-    print(snapshot.value);
+    FirebaseManager.auth.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginScreen()));
     setState(() {
       _counter++;
     });
