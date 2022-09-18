66e04a79f9c91dc5acc8ead4b4608b6e5b68c7dc
Krish Chopra <krishchopra22@gmail.com>
2022-09-18 12:16:10 +0530

    added something in main_dashboard

--- lib/widgets/what_for_dropdown.dart
+++ lib/widgets/what_for_dropdown.dart
@@ -1,67 +1,55 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-
-
-class WhatForDropdown extends StatefulWidget{
-
+class WhatForDropdown extends StatefulWidget {
   const WhatForDropdown({super.key});
-
-
 
   @override
   MyDropDown createState() => MyDropDown();
 }
 
-class MyDropDown extends State<WhatForDropdown>{
-
+class MyDropDown extends State<WhatForDropdown> {
   String? dropdownValue = 'Apple';
   @override
   Widget build(BuildContext context) {
+    final items = [
+      'Apple',
+      'Banana',
+      'Grapes',
+      'Orange',
+      'watermelon',
+      'Pineapple'
+    ];
 
-
-    final items =  ['Apple','Banana','Grapes','Orange','watermelon','Pineapple'];
-
-    return  Container(
-
+    return Container(
       padding: const EdgeInsets.all(10.0),
       child: Column(
-
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Row(
-
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-
               Container(
                 height: 40,
                 padding: const EdgeInsets.all(5.0),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(30.0),
                   border: Border.all(
-                      color: Colors.grey, style: BorderStyle.solid, width: 0.80),
+                      color: Colors.grey,
+                      style: BorderStyle.solid,
+                      width: 0.80),
                 ),
                 child: DropdownButtonHideUnderline(
                   child: DropdownButton(
-
                     icon: const Icon(Icons.keyboard_arrow_down),
-                    onChanged: (String? newValue){
+                    onChanged: (String? newValue) {
                       setState(() {
                         this.dropdownValue = newValue!;
                       });
                     },
                     value: dropdownValue,
-                    items:items.map((String items) {
-                      return DropdownMenuItem(
-                          value: items,
-                          child: Text(items)
-                      );
-                    }
-                    ).toList(),
-
-
-
+                    items: items.map((String items) {
+                      return DropdownMenuItem(value: items, child: Text(items));
+                    }).toList(),
                   ),
                 ),
               ),
@@ -69,12 +57,6 @@ class MyDropDown extends State<WhatForDropdown>{
           ),
         ],
       ),
-
     );
   }
-
-
-
 }
-
-
