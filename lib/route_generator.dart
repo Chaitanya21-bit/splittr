93cfed6d58c6b8609f0938cfee8d61c1951a53ec
Saurrabhh <saurabhkyadav99@gmail.com>
2023-02-07 20:49:08 +0530

    group provider fixed

--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -23,6 +23,7 @@ class RouteGenerator {
         return MaterialPageRoute(
             builder: (_) =>
              GroupDashboard(group: args as Group)
+
             );
       case '/quickSplit':
         return MaterialPageRoute(
