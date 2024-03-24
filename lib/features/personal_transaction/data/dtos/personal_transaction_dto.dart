74079a50ba6f71b56f0f98caa836a5058607c99c
Saurabh Yadav <saurabhkyadav99@gmail.com>
2024-03-25 05:10:02 +0530

    added personal transaction data classes

--- lib/features/personal_transaction/data/dtos/personal_transaction_dto.dart
+++ lib/features/personal_transaction/data/dtos/personal_transaction_dto.dart
@@ -1,18 +1,31 @@
-// import 'package:json_annotation/json_annotation.dart';
-//
-// part 'personal_transaction_dto.g.dart';
-//
-// @JsonSerializable()
-// class PersonalTransaction {
-//   final String? tid;
-//   final String? userId;
-//   final String? title;
-//   final double? amount;
-//   final DateTime date;
-//   final String? remarks;
-//   final String? category;
-//   factory PersonalTransaction.fromJson(Map<String, dynamic> json) =>
-//       _$PersonalTransactionFromJson(json);
-//
-//   Map<String, dynamic> toJson() => _$PersonalTransactionToJson(this);
-// }
+import 'package:json_annotation/json_annotation.dart';
+
+part 'personal_transaction_dto.g.dart';
+
+@JsonSerializable()
+class PersonalTransactionDto {
+  final String? transactionId;
+  final String? userId;
+  final String? title;
+  final double? amount;
+  final String? note;
+  final String? category;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+
+  const PersonalTransactionDto({
+    this.transactionId,
+    this.userId,
+    this.title,
+    this.amount,
+    this.note,
+    this.category,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  factory PersonalTransactionDto.fromJson(Map<String, dynamic> json) =>
+      _$PersonalTransactionDtoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PersonalTransactionDtoToJson(this);
+}
---
+++ lib/features/personal_transaction/domain/models/personal_transaction.dart
@@ -0,0 +1,46 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:splittr/features/personal_transaction/data/dtos/personal_transaction_dto.dart';
+
+part 'personal_transaction.freezed.dart';
+
+@freezed
+class PersonalTransaction with _$PersonalTransaction {
+  const factory PersonalTransaction({
+    String? transactionId,
+    String? userId,
+    String? title,
+    double? amount,
+    String? note,
+    String? category,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  }) = _PersonalTransaction;
+
+  const PersonalTransaction._();
+
+  factory PersonalTransaction.fromDto(PersonalTransactionDto dto) {
+    return PersonalTransaction(
+      transactionId: dto.transactionId,
+      userId: dto.userId,
+      title: dto.title,
+      amount: dto.amount,
+      note: dto.note,
+      category: dto.category,
+      createdAt: dto.createdAt,
+      updatedAt: dto.updatedAt,
+    );
+  }
+
+  PersonalTransactionDto toDto() {
+    return PersonalTransactionDto(
+      transactionId: transactionId,
+      userId: userId,
+      title: title,
+      amount: amount,
+      note: note,
+      category: category,
+      createdAt: createdAt,
+      updatedAt: updatedAt,
+    );
+  }
+}
